b5dbb84b092aae2887479cd5a4375505118762f5
Mesut OZTURK <mesut8311006@gmail.com>
2024-05-23 12:30:41 +0300

    apps/web header mobile fixed to responsive

--- apps/web/src/sections/header/HeaderSearch.tsx
+++ apps/web/src/sections/header/HeaderSearch.tsx
@@ -3,24 +3,36 @@ import Image from 'next/image'
 import { IconButton, Badge } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
 import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
+import Link from 'next/link';
 
 
 export const HeaderSearch = () => {
     return (
-        <div className='bg-white p-2 text-black'>
-            <div className='container flex items-center justify-between'>
-                <Image
-                    src="https://demo4techies.com/prestashop/shopkart-lite/img/shopkart-lite-logo-1561444415.jpg"
-                    alt=''
-                    width="180"
-                    height="105"
-                    // layout="responsive"
-                    // objectFit="contain"
-                    style={{ objectFit: "cover" }}
-                />
+        <div className='bg-white  text-black'>
+            <div className='container flex items-center justify-between p-2'>
+
+                <div className='flex items-center'>
+                    <IconButton className='flex sm:hidden'>
+                        <MenuIcon sx={{ cursor: 'pointer', color: '#666' }} />
+                    </IconButton>
+
+                    <Link href="/">
+                        <Image
+                            src="https://demo4techies.com/prestashop/shopkart-lite/img/shopkart-lite-logo-1561444415.jpg"
+                            alt=''
+                            width="100"
+                            height="100"
+                            // layout="responsive"
+                            // objectFit="contain"
+                            style={{ objectFit: "cover" }}
+                            className='w-[120px] sm:w-[180px]'
+                        />
+                    </Link>
+                </div>
 
                 <div className='flex items-center gap-5'>
-                    <div className='border-2 pr-1'>
+                    <div className='border-2 pr-1 hidden sm:block'>
                         <input
                             className='bg-white  p-3 text-[14px] outline-none'
                             placeholder='Search our catlog'
@@ -33,6 +45,18 @@ export const HeaderSearch = () => {
                     <Badge badgeContent={"0"} color="error">
                         <LocalMallOutlinedIcon sx={{ cursor: 'pointer', color: '#666' }} />
                     </Badge>
+                </div>
+            </div>
+
+            <div className='bg-[#f1f1f1] p-2'>
+                <div className='border-4 pr-1 flex sm:hidden bg-[#fff]'>
+                    <input
+                        className='bg-white  p-3 text-[14px] outline-none flex-1'
+                        placeholder='Search our catlog'
+                    />
+                    <IconButton sx={{}}>
+                        <SearchIcon />
+                    </IconButton>
                 </div>
             </div>
         </div>
